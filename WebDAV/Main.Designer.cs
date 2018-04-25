26ed1026a3320cd4a15cc9407b145522dc3e005f
AnnyShym <AnnyShym@gmail.com>
2018-04-25 17:06:01 +0300

    ready

---
+++ WebDAV/Main.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WebDAV
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.editHost = new System.Windows.Forms.TextBox();
+            this.editLogin = new System.Windows.Forms.TextBox();
+            this.editPassword = new System.Windows.Forms.TextBox();
+            this.editInfo = new System.Windows.Forms.TextBox();
+            this.labelHost = new System.Windows.Forms.Label();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.cbAction = new System.Windows.Forms.ComboBox();
+            this.labelAction = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.pbOK = new System.Windows.Forms.PictureBox();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // editHost
+            // 
+            this.editHost.Location = new System.Drawing.Point(30, 34);
+            this.editHost.Margin = new System.Windows.Forms.Padding(4);
+            this.editHost.Name = "editHost";
+            this.editHost.Size = new System.Drawing.Size(334, 22);
+            this.editHost.TabIndex = 4;
+            // 
+            // editLogin
+            // 
+            this.editLogin.Location = new System.Drawing.Point(31, 91);
+            this.editLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.editLogin.Name = "editLogin";
+            this.editLogin.Size = new System.Drawing.Size(334, 22);
+            this.editLogin.TabIndex = 5;
+            // 
+            // editPassword
+            // 
+            this.editPassword.Location = new System.Drawing.Point(31, 151);
+            this.editPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.editPassword.Name = "editPassword";
+            this.editPassword.Size = new System.Drawing.Size(334, 22);
+            this.editPassword.TabIndex = 6;
+            this.editPassword.UseSystemPasswordChar = true;
+            // 
+            // editInfo
+            // 
+            this.editInfo.Location = new System.Drawing.Point(30, 207);
+            this.editInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.editInfo.Name = "editInfo";
+            this.editInfo.Size = new System.Drawing.Size(334, 22);
+            this.editInfo.TabIndex = 7;
+            this.editInfo.Visible = false;
+            // 
+            // labelHost
+            // 
+            this.labelHost.AutoSize = true;
+            this.labelHost.Location = new System.Drawing.Point(27, 15);
+            this.labelHost.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelHost.Name = "labelHost";
+            this.labelHost.Size = new System.Drawing.Size(97, 17);
+            this.labelHost.TabIndex = 8;
+            this.labelHost.Text = "Host Address:";
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(28, 70);
+            this.labelLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(47, 17);
+            this.labelLogin.TabIndex = 9;
+            this.labelLogin.Text = "Login:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(27, 130);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(73, 17);
+            this.labelPassword.TabIndex = 10;
+            this.labelPassword.Text = "Password:";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(27, 186);
+            this.labelInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 17);
+            this.labelInfo.TabIndex = 11;
+            this.labelInfo.Visible = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // cbAction
+            // 
+            this.cbAction.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbAction.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbAction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAction.FormattingEnabled = true;
+            this.cbAction.Items.AddRange(new object[] {
+            "Make Collection",
+            "Delete File/Collection",
+            "Download File",
+            "Upload File"});
+            this.cbAction.Location = new System.Drawing.Point(42, 42);
+            this.cbAction.Name = "cbAction";
+            this.cbAction.Size = new System.Drawing.Size(334, 24);
+            this.cbAction.TabIndex = 12;
+            this.cbAction.SelectedIndexChanged += new System.EventHandler(this.actionComboBox_SelectedIndexChanged);
+            // 
+            // labelAction
+            // 
+            this.labelAction.AutoSize = true;
+            this.labelAction.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.labelAction.Location = new System.Drawing.Point(39, 18);
+            this.labelAction.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelAction.Name = "labelAction";
+            this.labelAction.Size = new System.Drawing.Size(51, 17);
+            this.labelAction.TabIndex = 13;
+            this.labelAction.Text = "Action:";
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.pbOK);
+            this.panel.Controls.Add(this.editInfo);
+            this.panel.Controls.Add(this.editHost);
+            this.panel.Controls.Add(this.editLogin);
+            this.panel.Controls.Add(this.labelInfo);
+            this.panel.Controls.Add(this.editPassword);
+            this.panel.Controls.Add(this.labelPassword);
+            this.panel.Controls.Add(this.labelHost);
+            this.panel.Controls.Add(this.labelLogin);
+            this.panel.Enabled = false;
+            this.panel.Location = new System.Drawing.Point(12, 88);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(394, 333);
+            this.panel.TabIndex = 14;
+            // 
+            // pbOK
+            // 
+            this.pbOK.BackgroundImage = global::WebDAV.Properties.Resources.ok;
+            this.pbOK.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbOK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbOK.Location = new System.Drawing.Point(171, 264);
+            this.pbOK.Name = "pbOK";
+            this.pbOK.Size = new System.Drawing.Size(52, 46);
+            this.pbOK.TabIndex = 12;
+            this.pbOK.TabStop = false;
+            this.pbOK.Click += new System.EventHandler(this.pbOK_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(418, 433);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.labelAction);
+            this.Controls.Add(this.cbAction);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cloud Client";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox editHost;
+        private System.Windows.Forms.TextBox editLogin;
+        private System.Windows.Forms.TextBox editPassword;
+        private System.Windows.Forms.TextBox editInfo;
+        private System.Windows.Forms.Label labelHost;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ComboBox cbAction;
+        private System.Windows.Forms.Label labelAction;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.PictureBox pbOK;
+    }
+}
+
---
+++ WebDAV/Main.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebDAV
+{
+    public partial class Main : Form
+    {
+
+        string host;
+        string login;
+        string password;
+        string info;
+
+        string clientPath;
+
+        const int bufferLength = 8000;
+
+        public Main()
+        {
+
+            InitializeComponent();
+
+            editHost.Text = "https://webdav.yandex.ru/";
+
+        }
+
+        private void actionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            panel.Enabled = true;
+
+            labelInfo.Visible = true;
+            editInfo.Visible = true;
+
+            switch (cbAction.SelectedIndex)
+            {
+
+                case 0:
+                    labelInfo.Text = "Collection Path:";
+                    break;
+
+                case 1:
+                    labelInfo.Text = "File/Collection Path:";
+                    break;
+
+                case 2:
+                    labelInfo.Text = "File's Path On Server";
+
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        clientPath = saveFileDialog.FileName;
+                    }
+                    saveFileDialog.Dispose();
+
+                    break;
+
+                case 3:
+                    labelInfo.Text = "File's Path On Server";
+
+                    OpenFileDialog openFileDialog = new OpenFileDialog();
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        clientPath = openFileDialog.FileName;
+                    }
+                    openFileDialog.Dispose();
+
+                    break;
+
+            }
+
+        }
+
+        private void pbOK_Click(object sender, EventArgs e)
+        {
+
+            host = editHost.Text;
+            login = editLogin.Text;
+            password = editPassword.Text;
+            info = editInfo.Text;
+
+            host += info;
+            host = host.TrimEnd();
+
+            switch (cbAction.SelectedIndex)
+            {
+
+                case 0:
+                    MakeCollection();
+                    break;
+
+                case 1:
+                    Delete();
+                    break;
+
+                case 2:
+                    DownloadFile();
+                    break;
+
+                case 3:
+                    UploadFile();
+                    break;
+
+            }
+        }
+
+        private void MakeCollection()
+        {
+            try
+            {
+
+                HttpWebRequest request = HttpWebRequest.Create(host) as HttpWebRequest;
+                request.Credentials = new NetworkCredential(login, password);
+                request.Method = WebRequestMethods.Http.MkCol;
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                HttpStatusCode code = response.StatusCode;
+
+                if (code == HttpStatusCode.Created)
+                {
+                    MessageBox.Show("The collection was created.");
+                }
+                else
+                {
+                    MessageBox.Show("Something got wrong. Please, try again later.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);   
+            }
+        }
+
+        private void Delete()
+        {
+            try
+            {
+
+                HttpWebRequest request = HttpWebRequest.Create(host) as HttpWebRequest;
+                request.Credentials = new NetworkCredential(login, password);
+                request.Method = "DELETE";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                HttpStatusCode code = response.StatusCode;
+
+                if (code == HttpStatusCode.NoContent)
+                {
+                    MessageBox.Show("The file/collection was deleted.");
+                }
+                else
+                {
+                    MessageBox.Show("Something got wrong. Please, try again later.");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DownloadFile()
+        {
+            try
+            {
+
+                HttpWebRequest request = HttpWebRequest.Create(host) as HttpWebRequest;
+                request.Credentials = new NetworkCredential(login, password);
+                request.Method = "GET";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                HttpStatusCode code = response.StatusCode;
+
+                byte[] buffer = new byte[bufferLength];
+                int bytesRead = 0;
+                long totalBytesRead = 0;
+
+                FileStream fileStream = new FileStream(clientPath, FileMode.Create, FileAccess.Write);
+                Stream responseStream = response.GetResponseStream();
+
+                do
+                {
+
+                    bytesRead = responseStream.Read(buffer, 0, bufferLength);
+                    if (bytesRead > 0)
+                    {
+                        totalBytesRead += bytesRead;
+                        fileStream.Write(buffer, 0, bytesRead);
+                    }
+
+                }
+                while (bytesRead > 0);
+
+                fileStream.Close();
+                responseStream.Close();
+
+                long contentLength = long.Parse(response.GetResponseHeader("Content-Length"));
+                if (totalBytesRead == response.ContentLength)
+                {
+                    MessageBox.Show("The file was downloaded succesfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Something got wrong. Please, try again later.");
+                }         
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void UploadFile()
+        {
+            try
+            {
+
+                HttpWebRequest request = HttpWebRequest.Create(host) as HttpWebRequest;
+                request.Credentials = new NetworkCredential(login, password);
+                request.Method = "PUT";
+                FileInfo fileInfo = new FileInfo(clientPath);
+                request.ContentLength = fileInfo.Length;
+                request.KeepAlive = false;
+                request.ReadWriteTimeout = -1;
+                request.Timeout = -1;
+                request.AllowWriteStreamBuffering = false;
+                request.PreAuthenticate = true;
+                request.SendChunked = false;
+                request.ContentType = "application/binary";
+                request.ProtocolVersion = HttpVersion.Version11;
+                request.ServicePoint.ConnectionLimit = 1;
+                request.AllowAutoRedirect = false;
+                request.ServicePoint.Expect100Continue = true;
+                request.Accept = "*/*";
+
+                byte[] buffer = new byte[bufferLength];
+                int bytesRead = 0;
+                long totalBytesRead = 0;
+
+                FileStream fileStream = new FileStream(clientPath, FileMode.Open, FileAccess.Read);
+                Stream requestStream = request.GetRequestStream();
+
+                do
+                {
+
+                    bytesRead = fileStream.Read(buffer, 0, bufferLength);
+                    if (bytesRead > 0)
+                    {
+                        totalBytesRead += bytesRead;
+                        requestStream.Write(buffer, 0, bytesRead);
+                    }
+
+                }
+                while (bytesRead > 0);
+
+                fileStream.Close();
+                requestStream.Close();
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                HttpStatusCode code = response.StatusCode;
+
+                if (code == HttpStatusCode.Created && totalBytesRead == request.ContentLength)
+                {
+                    MessageBox.Show("The file was uploaded succesfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Something got wrong. Please, try again later.");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
